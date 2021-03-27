fdacbe71fc4ea7601cddc72ce35580c7e3bdec4a
Viktor Vashchenko <viktor2236@gmail.com>
2021-03-27 10:36:45 +0200

    in progress

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-import user from './Profile/user.json';
+import Container from './Container/Container';
 import Profile from './Profile/Profile';
-
 import Statistics from './Statistics/Statistics';
-import statisticalData from './Statistics/statistical-data.json';
-
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+import user from './Profile/user.json';
+import statisticalData from './Statistics/statistical-data.json';
 import friends from './FriendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
 
 const App = () => {
   return (
-    <>
+    <Container>
       <Profile
         name={user.name}
         tag={user.tag}
@@ -19,12 +21,11 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
-
       <FriendList friends={friends} />
-    </>
+      <TransactionHistory items={transactions} />;
+    </Container>
   );
 };
 
